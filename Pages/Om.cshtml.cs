642ce80165f74a8c2dd4cae216c0afd4cbbfa661
Linus Hvenfelt <linushvenfelt@gmail.com>
2022-01-24 18:58:50 +0100

    page content

---
+++ Pages/Om.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moment_1.Pages
+{
+    public class OmModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 Run with ```dotnet run watch```
 
 New page: ```dotnet new page --name Razor --namespace Moment_1.Pages --output Pages```
+
+TODO:
+Göra CSS responsiv
+Lägg till någon javascript
