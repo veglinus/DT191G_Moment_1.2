1ef55900ddb2da3d4f419632b3f3a4fa8c8173e1
Linus Hvenfelt <linushvenfelt@gmail.com>
2022-01-24 19:07:58 +0100

    comic sans is king

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,6 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-
-
-// TODO: Implement some javascript to put here
+function comicSans() {
+    document.body.style.fontFamily = "Comic Sans MS", "Comic Sans";
+}
