57992819be9b8a405a6c2665c7f4e8e307461e8e
Linus Hvenfelt <linushvenfelt@gmail.com>
2022-01-24 17:59:31 +0100

    razor page

---
+++ Pages/Razor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moment_1.Pages
+{
+    public class RazorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
